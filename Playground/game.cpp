22e95fb7cf60d9396f5780e51bcc2f93b355a11c
DubcXD <122060438+DubcXD@users.noreply.github.com>
2023-01-16 05:24:21 +0000

    Mon, Jan 16, 2023, 5:24 AM +00:00

--- Playground/game.cpp
+++ Playground/game.cpp
@@ -1,12 +1,10 @@
 #include <string>
 #include <iostream>
 
-switch(choice)
-
 int main()
 {
     std::cout << "0. Quit" << std::endl << "1. Play Game\n";
-    int choice
+    int choice;
     std::cin >> choice;
 
     switch(choice)
