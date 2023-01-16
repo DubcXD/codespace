e958a3c711eb69366edf0a690607252766388212
DubcXD <122060438+DubcXD@users.noreply.github.com>
2023-01-16 05:29:02 +0000

    Mon, Jan 16, 2023, 5:29 AM +00:00

--- Playground/game.cpp
+++ Playground/game.cpp
@@ -3,17 +3,23 @@
 
 int main()
 {
-    std::cout << "0. Quit" << std::endl << "1. Play Game\n";
-    int choice;
-    std::cin >> choice;
 
-    switch(choice)
+    int choice;
+    do
     {
+        std::cout << "0. Quit" << std::endl << "1. Play Game\n";
+        std::cin >> choice;
+
+        switch(choice)
+        {
         case 0:
-        std::cout << "Why not? Games are fun\n";
-        return 0;
+            std::cout << "Why not? Games are fun\n";
+            return 0;
         case 1:
-        std::cout << "Yey, Let play!\n";
-        break;
+            std::cout << "Yey, Let play!\n";
+            break;
+        }
+
     }
+    while(choice != 0);
 }
